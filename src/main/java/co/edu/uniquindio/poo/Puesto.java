b0f5b09c2486d8bbef660ff31da60c63210e0cf3
walter1705 <waltergranada17@gmail.com>
2024-05-12 20:56:01 -0500

    Commit 03 correcion
    Correcion diagrama de clases, nueva ,logica

--- src/main/java/co/edu/uniquindio/poo/Puesto.java
+++ src/main/java/co/edu/uniquindio/poo/Puesto.java
@@ -1,27 +1,61 @@
 package co.edu.uniquindio.poo;
 
 public class Puesto {
-    private Posicion posicion;
-    private Vehiculo vehiculo;
+    private int i, j;
+    public boolean ocupado;
+    public Vehiculo vehiculo;
+    private Puesto[][] dimension;
     //Metodo constructor de la clase Puesto
-    public Puesto(Posicion posicion, Vehiculo vehiculo) {
-        this.posicion = posicion;
-        this.vehiculo = vehiculo;
+    public Puesto(int i, int j, boolean ocupado)  {
+        this.i = i;
+        this.j = j;
+        dimension = new Puesto[i][j];
+        this.ocupado = ocupado;
     }
-    //Metodo get y set de la posicion en la que esta la clase Puesto
-    public Posicion getPosicion() {
-        return posicion;
+    //Metodo get y set del estado del puesto
+    public boolean isOcupado() {
+        return ocupado;
     }
-    public void setPosicion(Posicion posicion) {
-        this.posicion = posicion;
+    public void setOcupado(boolean ocupado) {
+        this.ocupado = ocupado;
     }
-    //Metodo get y set del vehiculo que podria ocupar la clase Puesto
+    //Obtener y modificar el vehiculo que esta en el puesto
     public Vehiculo getVehiculo() {
         return vehiculo;
     }
     public void setVehiculo(Vehiculo vehiculo) {
         this.vehiculo = vehiculo;
     }
-
-    
+    //Metodo get y set de las FILAS de la dimension de los puestos del parqueadero
+    public int getI() {
+        return i;
+    }
+    public void setI(int i) {
+        this.i = i;
+    }
+    //Metodo get y set de las COLUMNAS de la dimension de los puestos del parqueadero
+    public int getJ() {
+        return j;
+    }
+    public void setJ(int j) {
+        this.j = j;
+    }
+    //Metodo get y set de la dimension de los puestos del parqueadero
+    public Puesto[][] getDimension() {
+        return dimension;
+    }
+    public void setDimension(Puesto[][] dimension) {
+        this.dimension = dimension;
+    }
+    //Metodo para ocupar un Puesto con un vehiculo
+    public void ocuparPuesto(Vehiculo vehiculo, int t, int w) {
+        assert(dimension[t][w].isOcupado()==false);
+        dimension[t][w].setVehiculo(vehiculo);
+        dimension[t][w].setOcupado(true);
+    }
+    //Metodo para obtener el propietario de un vehiculo en un puesto dado
+    public Propietario obtenerPropietarioPorPuesto(int t, int w) {
+        Propietario propietario=dimension[t][w].getVehiculo().getPropietario();
+        return propietario;
+    }
 }
